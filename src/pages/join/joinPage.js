e871eee60de3a855e601ae487932ca1085a3c99f
chaemin <w2114@e-mirim.hs.kr>
2023-08-20 02:43:43 +0900

    회원가입 폼 프론트 완성

--- src/pages/join/joinPage.js
+++ src/pages/join/joinPage.js
@@ -1,4 +1,40 @@
-const joinPage = () => {
-    return <div>회원가입</div>
+import "./joinPage.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const JoinPage = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+      id: "",
+      password: "",
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event) =>{
+        event.preventDefault();
+      
+    }
+  return (
+    <div>
+      <div className="join-box">
+        <div className="tab">
+            <div className="join">회원가입</div>
+        </div>
+        <form id="loginData" >
+          <div>아이디</div>
+          <input type="text" name="id" className="id" value={formData.id} onChange={handleChange} />
+          <div>비밀번호</div>
+          <input type="text" name="password" className="pw" value={formData.password} onChange={handleChange}/>
+          <input onClick={handleSubmit} className="join-btn" type="button" value="회원가입" />
+        </form>
+      </div>
+    </div>
+  );
 };
-export default joinPage;
+
+export default JoinPage;
