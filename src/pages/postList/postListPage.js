a91fe4a9f58de3dd08672bb566720faae8e092c4
chaemin <w2114@e-mirim.hs.kr>
2023-08-20 16:32:41 +0900

    글쓰기 버튼 완성

--- src/pages/postList/postListPage.js
+++ src/pages/postList/postListPage.js
@@ -1,9 +1,17 @@
 import Navbar from '../../components/Navbar';
-const postListPage = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./postListPage.css";
+import writeimg from "../../img/write.png";
+
+const PostListPage = () => {
     return (
       <div>
         <Navbar/>
+        <Link to="/write" className="write-button">
+          <img src={writeimg} ale="글쓰기 버튼"/>
+        </Link>
       </div>
     );
 };
-export default postListPage;
+export default PostListPage;
