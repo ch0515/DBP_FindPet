f90a23a2e73c34110aef2925a69dfd0dcdd499dd
chaemin <w2114@e-mirim.hs.kr>
2023-08-25 08:56:59 +0900

    사진 디비에 넣기

--- src/pages/write/writePage.js
+++ src/pages/write/writePage.js
@@ -8,44 +8,44 @@ import Post from '../../components/Popuppost';
 const WritePage = () => {
   const navigate = useNavigate();
     const [popup, setPopup] = React.useState(false);
-    const [animalType, setAnimalType] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [address, setAddress] = useState('');
-    const [photoFile, setPhotoFile] = useState(null);
-    const [memo, setMemo] = useState('');
     const [selectedImage, setSelectedImage] = useState(null); // 추가된 부분
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      // FormData 생성
-      const formData = new FormData();
-      formData.append('animalType', animalType);
-      formData.append('phoneNumber', phoneNumber);
-      formData.append('address', address);
-      formData.append('memo', memo);
-        if (photoFile) {
-          formData.append('photo', photoFile);
-        }else{
-      setSelectedImage(null); // 폼 제출 후 미리보기 이미지 초기화
-        }
-        
-      try{
-        const response = await axios.post('http://localhost:3001/post', formData, {
-          headers: {
-            'Content-Type' : 'multipart/form-data',
-          },
-        });
-        console.log('서버 응답 : ', response.data);
-        navigate("/")
-      }catch(error){
-        console.error('폼 제출 에러 : ', error);
-      }
+    
+    const [formData, setFormData] = useState({
+      animalType: "",
+      phoneNumber: "",
+      address: "",
+      photoFile: null,
+      memo: "",
+    });
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    };
+    const setAddress = (newAddress) => {
+      setFormData((prevData) => ({
+        ...prevData,
+        address: newAddress,
+      }));
+    };
+    const handleSubmit = (event) => {
+      event.preventDefault(); // 기본 제출 동작 방지
+        axios.post("http://localhost:3002/write", formData).then((response) => {
+          console.log('서버 응답 : ', response.data);
+          alert("글 작성 완료");
+        navigate("/");
+        })
+      .catch(error => {
+        console.log('폼 제출 에러 : ', error);
+        alert("글 작성 실패");
+      });
     };
   
     const handlePhotoChange = (event) => {
       const selectedFile = event.target.files[0];
-      setPhotoFile(selectedFile);
+      //setPhotoFile(selectedFile);
       //setSelectedImage(URL.createObjectURL(selectedFile)); // 미리보기 이미지 설정
       if (selectedImage) {
         URL.revokeObjectURL(selectedImage);
@@ -60,26 +60,32 @@ const WritePage = () => {
     return (
         <div className='write-form'>
         <h2 className='write-name'>글 쓰기</h2>
-        <form onSubmit={handleSubmit} className='form-style'>
+        <form className='form-style'>
           <div className='animal-group'>
             <label className='animal-type-name'>동물 종류</label>
-            <input className='animal-type-form' type="text" value={animalType} onChange={(e) => setAnimalType(e.target.value)} required/>
+            <input className='animal-type-form' type="text" name="animalType" value={formData.animalType} onChange={handleChange} required/>
           </div>
           <div className='tel-group'>
             <label className='tel-form-name'>전화번호</label>
-            <input className='tel-form' type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required/>
+            <input className='tel-form' type="tel" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required/>
           </div>
           <div className='address-group'>
           <label className='address-serch' onClick={()=>{setPopup(!popup)}}>🔍︎ 주소 검색</label>
                {
                  popup && 
-                    <span><Post address={address} setAddress={setAddress}></Post></span>
+                    <span><Post address={formData.address} setAddress={setAddress}></Post></span>
                     }
-            <span className='address-name'>{address}</span>
+            <input
+            name='address'
+  className='address-name'
+  type="text"
+  value={formData.address}
+  readOnly // 주소 입력 필드를 읽기 전용으로 설정
+/>
           </div>
           <div className='memo-group'>
             <label className='memo-name'>메모</label>
-            <textarea className='memo-form' cols="50" rows="10" value={memo} onChange={(e) => setMemo(e.target.value)} required/>
+            <textarea className='memo-form' name="memo" cols="50" rows="10" value={formData.memo} onChange={handleChange} required/>
           </div>
           <div className='img-group'>
             <label className='img-name'>사진 업로드</label>
@@ -91,7 +97,7 @@ const WritePage = () => {
               <img src={selectedImage} alt="미리보기"style={{maxWidth: '200px', maxHeight: '150px'}}/>
             </div>
           )}
-          <button type="submit" className='form-submit'>글 작성</button>
+          <button onClick={handleSubmit} type="submit" className='form-submit'>글 작성</button>
         </form>
       </div>
     );
