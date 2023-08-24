b617f878ef3e930f195d5e479809755d5fd14577
chaemin <w2114@e-mirim.hs.kr>
2023-08-24 09:04:41 +0900

    사용자가 로그인 했을 때만 글을 쓸 수 있게 수정

--- src/pages/postList/postListPage.js
+++ src/pages/postList/postListPage.js
@@ -1,17 +1,29 @@
 import Navbar from '../../components/Navbar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import "./postListPage.css";
 import writeimg from "../../img/write.png";
 
 const PostListPage = () => {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const storedUserData = sessionStorage.getItem("userData");
+        if (storedUserData) {
+            setUserData(JSON.parse(storedUserData));
+        }
+    }, []);
+
     return (
-      <div>
-        <Navbar/>
-        <Link to="/write" className="write-button">
-          <img src={writeimg} ale="글쓰기 버튼"/>
-        </Link>
-      </div>
+        <div>
+            <Navbar/>
+            {userData && (
+                <Link to="/write" className="write-button">
+                    <img src={writeimg} alt="글쓰기 버튼"/>
+                </Link>
+            )}
+        </div>
     );
 };
+
 export default PostListPage;
