3445dbe1f4cb9ccf5e5c85b1d54e1e22700197c9
chaemin <w2114@e-mirim.hs.kr>
2023-08-27 01:00:00 +0900

    게시판 데이터 디비에 저장 성공

--- src/pages/write/writePage.js
+++ src/pages/write/writePage.js
@@ -31,32 +31,56 @@ const WritePage = () => {
       }));
     };
     const handleSubmit = (event) => {
-      event.preventDefault(); // 기본 제출 동작 방지
-        axios.post("http://localhost:3002/write", formData).then((response) => {
-          console.log('서버 응답 : ', response.data);
-          alert("글 작성 완료");
-        navigate("/");
-        })
-      .catch(error => {
-        console.log('폼 제출 에러 : ', error);
-        alert("글 작성 실패");
-      });
+      event.preventDefault();
+
+  const formDataToSend = new FormData();
+  formDataToSend.append('animalType', formData.animalType);
+  formDataToSend.append('phoneNumber', formData.phoneNumber);
+  formDataToSend.append('address', formData.address);
+  formDataToSend.append('photoFile', formData.photoFile);
+  formDataToSend.append('memo', formData.memo);
+
+  axios.post("http://localhost:3002/write", formDataToSend, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  }).then((response) => {
+    console.log('서버 응답 : ', response.data);
+    alert("글 작성 완료");
+    navigate("/");
+  }).catch(error => {
+    console.log('폼 제출 에러 : ', error);
+    alert("글 작성 실패");
+  });
     };
   
-    const handlePhotoChange = (event) => {
-      const selectedFile = event.target.files[0];
-      //setPhotoFile(selectedFile);
-      //setSelectedImage(URL.createObjectURL(selectedFile)); // 미리보기 이미지 설정
-      if (selectedImage) {
-        URL.revokeObjectURL(selectedImage);
-      }
-  
-      if (selectedFile) {
-        setSelectedImage(URL.createObjectURL(selectedFile)); // 미리보기 이미지 설정
-      } else {
-        setSelectedImage(null);
-      }
+    const [previewImage, setPreviewImage] = useState(null);
+
+const handlePhotoChange = async (event) => {
+  const selectedFile = event.target.files[0];
+  const arrayBuffer = await selectedFile.arrayBuffer();
+  const hexString = Array.from(new Uint8Array(arrayBuffer))
+    .map(byte => byte.toString(16).padStart(2, '0'))
+    .join('');
+  if (selectedFile) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const imageData = e.target.result;
+      setFormData((prevData) => ({
+        ...prevData,
+        photoFile: selectedFile,
+      }));
+      setPreviewImage(imageData); // 미리보기 이미지 설정
     };
+    reader.readAsDataURL(selectedFile);
+  } else {
+    setFormData((prevData) => ({
+      ...prevData,
+      photoFile: null,
+    }));
+    setPreviewImage(null);
+  }
+};
     return (
         <div className='write-form'>
         <h2 className='write-name'>글 쓰기</h2>
@@ -89,14 +113,14 @@ const WritePage = () => {
           </div>
           <div className='img-group'>
             <label className='img-name'>사진 업로드</label>
-            <input className='img-form' type="file" accept="image/*" onChange={handlePhotoChange}/>
+            <input name='photoFile' className='img-form' type="file" accept="image/*" onChange={handlePhotoChange}/>
           </div>
-          {selectedImage && (
-            <div>
-              <label className='img-preview'>미리보기</label>
-              <img src={selectedImage} alt="미리보기"style={{maxWidth: '200px', maxHeight: '150px'}}/>
-            </div>
-          )}
+          {previewImage && (
+  <div>
+    <label className='img-preview'>미리보기</label>
+    <img src={previewImage} alt="미리보기" style={{maxWidth: '200px', maxHeight: '150px'}}/>
+  </div>
+)}
           <button onClick={handleSubmit} type="submit" className='form-submit'>글 작성</button>
         </form>
       </div>
