7012b337db353416cbf96b75bf984c39cc5e5d65
chaemin <w2114@e-mirim.hs.kr>
2023-08-30 01:46:15 +0900

    일단 커밋시켜놓기

--- src/pages/postList/postListPage.js
+++ src/pages/postList/postListPage.js
@@ -10,7 +10,7 @@ const PostListPage = () => {
     const [userData, setUserData] = useState(null);
     const [posts, setPosts] = useState([]);
     const [, setForceRender] = useState();
-    const [imageUrls, setImageUrls] = useState([]);
+    const [imageSrc, setImageSrc] = useState('');
 
     useEffect(() => {
         const storedUserData = sessionStorage.getItem("userData");
@@ -18,13 +18,14 @@ const PostListPage = () => {
             setUserData(JSON.parse(storedUserData));
         }
 
-        axios.get("http://localhost:3002/images")
-      .then(response => {
-        setImageUrls(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching image URLs:', error);
-      });
+        axios.get('http://localhost:3002/image', { responseType: 'arraybuffer' })
+            .then(response => {
+                const imageBlob = new Blob([response.data], { type: 'image/jpeg' }); // 이미지 타입에 맞게 설정
+                setImageSrc(URL.createObjectURL(imageBlob));
+            })
+            .catch(error => {
+                console.error('이미지 가져오기 실패', error);
+            });
         axios.get("http://localhost:3002/posts")
             .then(response => {
                 const dataFromServer = response.data;
@@ -49,10 +50,9 @@ const PostListPage = () => {
             <div className="post-box">
                 <div className='post-grid'>
                     {posts.map(post => (
-                        <div key={post} className="post-list">
+                        <div key={post[0]} className="post-list">
                             {/* <img src={`http://localhost:3002/images/${post[3]}`} alt="Post Image"/> */}
-                            
-                            <ImageViewer imageUrls={imageUrls} alt={`Post ${post}`} width="100" height="100" />
+                            <img src={imageSrc} alt="이미지" />
                             <p>전화번호 : {post[0]}</p>
                             <p>종류 : {post[1]}</p>
                             <p>주소 : {post[2]}</p>
